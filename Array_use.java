380722ae04fc5d7db5d76cbea1f23978a4e8a1f2
JPMCjava <107176849+JPMCjava@users.noreply.github.com>
2022-06-16 22:15:35 -0800

    Add files via upload

---
+++ Array_use.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class Array_use {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Arr_demo1 obj=new Arr_demo1();
+		//obj.ascending();
+		//obj.descending();
+		//obj.greater();
+		//obj.smaller();
+		obj.accepting_array();
+		
+	}
+}
